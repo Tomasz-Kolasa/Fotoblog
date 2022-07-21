4448a15ef61f8385720beee2a7194801cf8afac3
Tom K <kasky@wp.pl>
2022-07-21 19:51:23 +0200

    Database connection set

---
+++ Fotoblog.DAL/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Fotoblog.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fotoblog.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<TagEntity> TagEntities { get; set; }
+        public ApplicationDbContext( DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Fotoblog.DAL/Entities/TagEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Fotoblog.DAL.Entities
+{
+    public class TagEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
