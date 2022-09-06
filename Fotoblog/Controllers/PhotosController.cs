f6891a3d524390a39b6c2c563126d7da1b304705
Tom K <kasky@wp.pl>
2022-09-06 20:33:23 +0200

    ImgController added, working on HomeView

--- Fotoblog/Controllers/PhotosController.cs
+++ Fotoblog/Controllers/PhotosController.cs
@@ -1,5 +1,6 @@
 ﻿using Fotoblog.BLL.Services.PhotosService;
 using Fotoblog.Utils.ViewModels.Photos;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fotoblog.Controllers
@@ -7,11 +8,18 @@ namespace Fotoblog.Controllers
     public class PhotosController : BaseController
     {
         private readonly IPhotoService _photoService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IServer _server;
 
-        public PhotosController(IPhotoService photoService, IWebHostEnvironment webHostEnvironment)
+        public PhotosController(IPhotoService photoService,
+            IWebHostEnvironment webHostEnvironment,
+            IServer server)
         {
+            _webHostEnvironment = webHostEnvironment;
             _photoService = photoService;
-            _photoService.SetLocation(webHostEnvironment.ContentRootPath);
+            _server = server;
+            //var addresses = _server.Features.Get<IServerAddressesFeature>().Addresses;
+            _photoService.SetLocation(_webHostEnvironment.ContentRootPath);
         }
 
         [HttpPost]
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,33 +1,45 @@
 <template>
     <v-container>
       <v-row>
-    <v-col
-      v-for="n in 9"
-      :key="n"
-      class="d-flex child-flex"
-      cols="6"
-    >
-      <v-img
-        :src="link[0]"
-        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-        aspect-ratio="16/9"
-        class="grey lighten-2"
-      >
-        <template v-slot:placeholder>
-          <v-row
-            class="fill-height ma-0"
-            align="center"
-            justify="center"
+        <v-col
+        class="d-flex child-flex"
+        cols="6">
+
+          <v-img
+            src="https://localhost:5000/Img/6f7d29e4-e9c0-4cd0-8955-a48582ac4882/orginal.jpg"
+            :lazy-src="`https://picsum.photos/10/6?image=${5 + 10}`"
+            aspect-ratio="16/9"
+            class="grey lighten-2">
+          </v-img>
+
+        </v-col>
+        <v-col
+          v-for="n in 9"
+          :key="n"
+          class="d-flex child-flex"
+          cols="6"
+        >
+          <v-img
+            :src="link[0]"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            aspect-ratio="16/9"
+            class="grey lighten-2"
           >
-            <v-progress-circular
-              indeterminate
-              color="grey lighten-5"
-            ></v-progress-circular>
-          </v-row>
-        </template>
-      </v-img>
-    </v-col>
-  </v-row>
+            <template v-slot:placeholder>
+              <v-row
+                class="fill-height ma-0"
+                align="center"
+                justify="center"
+              >
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </v-col>
+      </v-row>
     </v-container>
 </template>
 
